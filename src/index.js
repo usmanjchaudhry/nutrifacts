2f4b2b25bc50b3fd306e047e24491e24266eb839
usmanjchaudhry <36524989+usmanjchaudhry@users.noreply.github.com>
2022-12-16 11:21:30 -0800

    Added route elements

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import App from './App';
+import Projects from "./routes/projects";
+import About from "./routes/about";
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <HashRouter >
+    <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="projects" element={<Projects />} />
+          <Route path="about" element={<About />} />
+        </Route>
+    </Routes>
+  </HashRouter>
+);
