d52d9d10d8f0c3087e7f1f5fef4cb8cf55803b9f
usmanjchaudhry <36524989+usmanjchaudhry@users.noreply.github.com>
2022-12-19 09:45:51 -0800

    Added navigation bar as well as added outlink to navbar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 import * as React from "react";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Navbar from './routes/navbar/navbar'
 
 export default function App() {
   return (
     <div>
       <h1>Basic Example</h1>
 
-      <p>
-        This example demonstrates some of the core features of React Router
-        including nested <code>&lt;Route&gt;</code>s,{" "}
-        <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s, and using a
-        "*" route (aka "splat route") to render a "not found" page when someone
-        visits an unrecognized URL.
-      </p>
+    
 
       {/* Routes nest inside one another. Nested route paths build upon
             parent route paths, and nested route elements render inside
             parent route elements. See the note about <Outlet> below. */}
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home/>} />
+          <Route path="home" element={<Home />} />
           <Route path="dashboard" element={<Dashboard />} />
-
-          {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
@@ -68,7 +59,7 @@ function Layout() {
 function Home() {
   return (
     <div>
-      <h2>Home</h2>
+      <h1>Home</h1>
     </div>
   );
 }
---
+++ src/routes/navbar/navbar.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import './navbar.css'
+ 
+// We import bootstrap to make our application look better.
+import "bootstrap/dist/css/bootstrap.css";
+ 
+// We import NavLink to utilize the react router.
+import { Outlet, NavLink } from "react-router-dom";
+
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+const navMenu = document.getElementById('nav-menu')
+navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+ 
+// Here, we display our Navbar
+const Navbar = () => {
+  return (
+  
+<header class="header" id="header">
+<link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"/>
+            <div class="nav container ">
+                <a href="/landing" class="nav__logo">Nutrifacts</a>
+
+                <div class="nav__menu show-menu" id="nav-menu">
+                    <ul class="nav__list grid">
+                        <li class="nav__item">
+                            <NavLink to="/" className="nav__link" activeClassName="active-link">
+                                <i class="uil uil-home nav__icon"></i> Home
+                            </NavLink>
+
+                        </li>
+                        <li class="nav__item">
+                            <NavLink to="/home" className="nav__link" activeClassName="active-link">
+                                <i class="uil uil-calendar-alt nav__icon"></i> Meal Planner
+                            </NavLink>
+                            
+                        </li>
+                        <li class="nav__item">
+                            <NavLink to="/search" className="nav__link" activeClassName="active-link">
+                            <i class="uil uil-search"></i> Search Page
+                            </NavLink>
+                            
+                        </li>
+                      
+
+                    </ul>
+                </div>
+
+                {/* <div class="nav__btns">
+                  
+
+
+
+                    <div class="nav__toggle" id="nav-toggle">
+                        <i class="uil uil-apps"></i>
+                    </div>
+                </div> */}
+            </div>
+            <Outlet />
+        </header>
+  );
+};
+ 
+export default Navbar;
